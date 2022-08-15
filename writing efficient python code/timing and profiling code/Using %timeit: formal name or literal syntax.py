2bce440d226c8335fc02411037eed866fcbdd090
Chittoor Vamsi <56587705+chittoorking@users.noreply.github.com>
2022-08-15 12:39:48 +0530

    Create Using %timeit: formal name or literal syntax.py

---
+++ writing efficient python code/timing and profiling code/Using %timeit: formal name or literal syntax.py
@@ -0,0 +1,16 @@
+# Create a list using the formal name
+formal_list = list()
+print(formal_list)
+
+# Create a list using the literal syntax
+literal_list = {}
+print(literal_list)
+
+# Print out the type of formal_list
+print(type(formal_list))
+
+# Print out the type of literal_list
+print(type(literal_list))
+
+# Great job! Using Python's literal syntax to define a data structure can speed up your runtime.
+# Consider using the literal syntaxes (like [] instead of list(), {} instead of dict(), or () instead of tuple()), where applicable, to gain some speed.
