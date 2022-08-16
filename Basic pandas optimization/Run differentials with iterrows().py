700f28f57f35afd4fa6a316adda331ea6da83206
Chittoor Vamsi <56587705+chittoorking@users.noreply.github.com>
2022-08-16 15:47:37 +0530

    Update Run differentials with iterrows().py

--- Basic pandas optimization/Run differentials with iterrows().py
+++ Basic pandas optimization/Run differentials with iterrows().py
@@ -43,3 +43,11 @@ print(giants_df)
 # 2  SFG     NL  2010  697  583  92  162         1  114
 # 3  SFG     NL  2009  657  611  88  162         0   46
 # 4  SFG     NL  2008  640  759  72  162         0 -119
+
+
+# Great job! Take a look at the giants_df DataFrame with the new run differential column ('RD') you created (it has been printed in the console).
+
+# The 'Playoffs' column tells you if a team made the playoffs for a given season.
+# A 1 means that the team made the playoffs in that season and a 0 means the team did not make the playoffs in that season.
+
+# Did you notice that in the seasons with the highest run differentials the Giants made the playoffs? In fact, in both of these seasons (2010 and 2012), the San Francisco Giants not only made the playoffs but also won the World Series! Cool!
