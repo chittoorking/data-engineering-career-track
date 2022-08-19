3b6dc6a80c0c360ce65925fb42e17be2ac2452aa
Chittoor Vamsi <56587705+chittoorking@users.noreply.github.com>
2022-08-19 12:11:44 +0530

    Create Best practice for default arguments.py

---
+++ writing functions in python/best_practices/Best practice for default arguments.py
@@ -0,0 +1,22 @@
+# Use an immutable variable for the default argument
+def better_add_column(values, df=None):
+  """Add a column of `values` to a DataFrame `df`.
+  The column will be named "col_<n>" where "n" is
+  the numerical index of the column.
+
+  Args:
+    values (iterable): The values of the new column
+    df (DataFrame, optional): The DataFrame to update.
+      If no DataFrame is passed, one is created by default.
+
+  Returns:
+    DataFrame
+  """
+  # Update the function to create a default DataFrame
+  if df is None:
+    df = pandas.DataFrame()
+  df['col_{}'.format(len(df.columns))] = values
+  return df
+
+# Beautiful and best practice! When you need to set a mutable variable as a default argument, always use None and then set the value in the body of the function.
+This prevents unexpected behavior like adding multiple columns if you call the function more than once.
