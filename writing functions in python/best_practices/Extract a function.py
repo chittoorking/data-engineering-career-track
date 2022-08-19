2651b53b3f4115786d656f79a5c7e0ee1c8201be
Chittoor Vamsi <56587705+chittoorking@users.noreply.github.com>
2022-08-19 11:52:43 +0530

    Create Extract a function.py

---
+++ writing functions in python/best_practices/Extract a function.py
@@ -0,0 +1,21 @@
+def standardize(column):
+  """Standardize the values in a column.
+
+  Args:
+    column (pandas Series): The data to standardize.
+
+  Returns:
+    pandas Series: the values as z-scores
+  """
+  # Finish the function so that it returns the z-scores
+  z_score = (column - column.mean()) / column.std()
+  return z_score
+
+
+# Finish the function so that it returns the z-scores of a column.
+# Use the function to calculate the z-scores for each year (df['y1_z'], df['y2_z'], etc.) from the raw GPA scores (df.y1_gpa, df.y2_gpa, etc.).
+# Use the standardize() function to calculate the z-scores
+df['y1_z'] = standardize(df.y1_gpa)
+df['y2_z'] = standardize(df.y2_gpa)
+df['y3_z'] = standardize(df.y3_gpa)
+df['y4_z'] = standardize(df.y4_gpa)
