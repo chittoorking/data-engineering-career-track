100cffff5392381bc37f2cf3f3c14297bcb94da4
Chittoor Vamsi <56587705+chittoorking@users.noreply.github.com>
2022-08-19 12:21:59 +0530

    Create The number of cats.py

---
+++ writing functions in python/context managers/The number of cats.py
@@ -0,0 +1,12 @@
+# Open "alice.txt" and assign the file to "file"
+with open('alice.txt') as file:
+  text = file.read()
+
+n = 0
+for word in text.split():
+  if word.lower() in ['cat', 'cats']:
+    n += 1
+
+print('Lewis Carroll uses the word "cat" {} times'.format(n))
+
+# Lewis Carroll uses the word "cat" 24 times
