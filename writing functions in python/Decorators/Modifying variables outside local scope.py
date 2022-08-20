d94196cb63dc280d221a13924579236e8f7aa46b
Chittoor Vamsi <56587705+chittoorking@users.noreply.github.com>
2022-08-20 14:57:31 +0530

    Update Modifying variables outside local scope.py

--- writing functions in python/Decorators/Modifying variables outside local scope.py
+++ writing functions in python/Decorators/Modifying variables outside local scope.py
@@ -18,3 +18,41 @@ def my_function():
   
 for _ in range(20):
   my_function()
+
+  
+#   Add a keyword that lets us modify file_contents from inside save_contents().
+
+def read_files():
+  file_contents = None
+  
+  def save_contents(filename):
+    # Add a keyword that lets us modify file_contents
+    nonlocal file_contents
+    if file_contents is None:
+      file_contents = []
+    with open(filename) as fin:
+      file_contents.append(fin.read())
+      
+  for filename in ['1984.txt', 'MobyDick.txt', 'CatsEye.txt']:
+    save_contents(filename)
+    
+  return file_contents
+
+print('\n'.join(read_files()))
+
+# Add a keyword to done in check_is_done() so that wait_until_done() eventually stops looping.
+def wait_until_done():
+  def check_is_done():
+    # Add a keyword so that wait_until_done() 
+    # doesn't run forever
+    global done
+    if random.random() < 0.1:
+      done = True
+      
+  while not done:
+    check_is_done()
+
+done = False
+wait_until_done()
+
+print('Work done? {}'.format(done))
